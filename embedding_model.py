5affe4f410bc3d7f4ed6a0356de9ab7300d69470
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2024-07-01 20:42:42 +0530

    Update embedding_model.py

--- embedding_model.py
+++ embedding_model.py
@@ -1,5 +1,7 @@
+#importing libraries
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 
+#embeding functions that will use for embeding both time
 def get_embedding_function():
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
