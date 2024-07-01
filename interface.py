8939bb2a52ef67074692d3be3c268129464ab586
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2024-07-01 20:55:29 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,8 +1,12 @@
+#importing libraries
 import streamlit as st
 from query_engine import query_rag
 
+#giving the title
 st.title("Content Engine - RAG")
 
+
+#making functional buttons
 query = st.text_input("Enter your query:")
 if st.button("Submit"):
     if query:
