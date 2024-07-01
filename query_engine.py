b247d327bcf54e2d22b7488aa9c5e3598591285b
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2024-07-01 20:58:37 +0530

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -5,8 +5,8 @@ from langchain_community.llms.ollama import Ollama
 from embedding_model import get_embedding_function
 from vector_store import initialize_vector_store, add_documents_to_store
 
-
-CHROMA_PATH = r"C:\Users\rhyth\content-engine\chroma"
+#mentioning path
+CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -17,23 +17,7 @@ Answer the question based only on the following context:
 
 Answer the question based on the above context: {question}
 """
-
-# def query_rag(query_text: str):
-#     embedding_function = get_embedding_function()
-#     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-#     results = db.similarity_search_with_score(query_text, k=5)
-
-#     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-#     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-#     prompt = prompt_template.format(context=context_text, question=query_text)
-
-#     model = Ollama(model="llama2")
-#     response_text = model.invoke(prompt)
-
-#     sources = [doc.metadata.get("id", None) for doc, _score in results]
-#     formatted_response = f"Response: {response_text}\nSources: {sources}"
-#     print(formatted_response)
-#     return response_text
+#making function to make query
 def query_rag(query_text: str):
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
