b932d069113a4923e3b58c1d077eeb70fcbf4e42
Rhythm Bellic <92723976+Rhythmbellic@users.noreply.github.com>
2024-07-01 20:40:29 +0530

    Update document_processing.py

--- document_processing.py
+++ document_processing.py
@@ -1,13 +1,16 @@
+#importing libraries
 from langchain.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain.schema.document import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-DATA_PATH = r"C:\Users\rhyth\content-engine\data"
+DATA_PATH = "data"
 
+#function to load the data
 def load_documents():
     document_loader = PyPDFDirectoryLoader(DATA_PATH)
     return document_loader.load()
 
+#function to split the document
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=800,
